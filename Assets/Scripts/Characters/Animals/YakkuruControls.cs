300689619bc5fbcb033cfa462c659dee46e8bc20
FishyTree <53624883+camcartier@users.noreply.github.com>
2025-03-25 21:54:13 +0100

    The return of the confused
    A small update on the yakkurus because I need to start back up easy after such a long hiatus. They now have a timer set up for idle behaviour. But they don't move yet, they just debug and change color. Yay

--- Assets/Scripts/Characters/Animals/YakkuruControls.cs
+++ Assets/Scripts/Characters/Animals/YakkuruControls.cs
@@ -6,6 +6,16 @@ using NaughtyAttributes;
 
 public class YakkuruControls : MonoBehaviour
 {
+    [Header("Idle")]
+    private bool isWaiting;
+    private float waitingTimer = 4;
+    private float waitingTimerCounter;
+    private bool waitingTimerStarted;
+    private float walkingTimer = 3;
+    private float walkingTimerCounter;
+    private bool walkingTimerStarted;
+    private List<Vector2> cardinalDirections;
+
     [Header ("Fleeing")]
     private bool isFleeing;
     private bool hasFleeingDirection;
@@ -14,13 +24,18 @@ public class YakkuruControls : MonoBehaviour
     [SerializeField] float runningTimer;
     private float runningCounter;
 
+
     private Destroyable destroyable;
     private Rigidbody2D rb2;
     private Animator animator;
+    private SpriteRenderer spriteRenderer;
+
     private GameObject bossYakkuruSpawner;
+
 
     [Expandable] [SerializeField] ObjectsData yakkuruData;
     [SerializeField] GameObject player;
+
 
     private void OnValidate()
     {
@@ -35,18 +50,64 @@ public class YakkuruControls : MonoBehaviour
         animator = GetComponentInChildren<Animator>();
         bossYakkuruSpawner = GameObject.Find("BossYakkuruSpawner");
 
-        //spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+        spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+
 
         if (player == null) { player = GameObject.Find("Player"); }
+
+        //cardinalDirections.Add(new Vector2(0, 1));
+        //cardinalDirections.Add(new Vector2(0, -1));
+        //cardinalDirections.Add(new Vector2(1, 0));
+        //cardinalDirections.Add(new Vector2(-1, 0));
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (isWaiting)
+        {
+            waitingTimerStarted= true;
+            spriteRenderer.color= Color.white;
+        }
+        else
+        {
+            walkingTimerStarted= true;
+            spriteRenderer.color = Color.yellow;
+        }
+        
+        if (waitingTimerStarted)
+        {
+            waitingTimerCounter += Time.deltaTime;
+            Debug.Log("I am waiting");
+            if(waitingTimerCounter > waitingTimer) 
+            {
+                isWaiting= false;
+                waitingTimerStarted= false;
+                walkingTimerStarted= true;
+                waitingTimerCounter= 0;
+            }
+        }
+
+        if (walkingTimerStarted)
+        {
+            walkingTimerCounter += Time.deltaTime;
+            Debug.Log("I am walking");
+            if (walkingTimerCounter > walkingTimer)
+            {
+                isWaiting= true;
+                walkingTimerStarted= false;
+                waitingTimerStarted= true;
+                walkingTimerCounter= 0;
+            }
+        }
+
 
         if (isFleeing) 
         {
             timerStarted = true;
+            //isWalking = false;
+            isWaiting = false; 
+            spriteRenderer.color= Color.white;
 
             if (!hasFleeingDirection)
             {
@@ -56,7 +117,7 @@ public class YakkuruControls : MonoBehaviour
                 animator.SetBool("isRunning", true);
             }
 
-            rb2.velocity = fleeingDirection*yakkuruData.runSpeed;
+            rb2.velocity = fleeingDirection*yakkuruData.runSpeed*Time.deltaTime;
             if (fleeingDirection.x < 0)
             {
                 gameObject.transform.localScale = new Vector3(-1, 1, 1); 
@@ -76,8 +137,15 @@ public class YakkuruControls : MonoBehaviour
                 runningCounter = 0;
                 rb2.velocity = Vector2.zero;
                 animator.SetBool("isRunning", false);
+                isWaiting= true;
             }
         }
+    }
+
+    private Vector2 RandomCardinalDirection()
+    {
+        int randomIndex = Random.Range(0, 3);
+        return new Vector2(cardinalDirections[randomIndex].x, cardinalDirections[randomIndex].y);
     }
 
 
@@ -92,6 +160,6 @@ public class YakkuruControls : MonoBehaviour
 
     private void OnDestroy()
     {
-        bossYakkuruSpawner.GetComponent<BossYakkuruSpawner>().YakkurusKilled += 1;
+        //bossYakkuruSpawner.GetComponent<BossYakkuruSpawner>().YakkurusKilled += 1;
     }
 }
