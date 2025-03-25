c34ef25a89b39037264796604992cda9234ad661
FishyTree <53624883+camcartier@users.noreply.github.com>
2025-03-25 22:39:52 +0100

    Update YakkuruControls.cs

--- Assets/Scripts/Characters/Animals/YakkuruControls.cs
+++ Assets/Scripts/Characters/Animals/YakkuruControls.cs
@@ -14,6 +14,8 @@ public class YakkuruControls : MonoBehaviour
     private float walkingTimer = 3;
     private float walkingTimerCounter;
     private bool walkingTimerStarted;
+    private bool hasDirection;
+    private Vector2 walkingDirection;
     private List<Vector2> cardinalDirections;
 
     [Header ("Fleeing")]
@@ -24,11 +26,14 @@ public class YakkuruControls : MonoBehaviour
     [SerializeField] float runningTimer;
     private float runningCounter;
 
-
+    [Header ("General")]
     private Destroyable destroyable;
     private Rigidbody2D rb2;
     private Animator animator;
     private SpriteRenderer spriteRenderer;
+    private bool gameJustStarted;
+    private float startGameWaitingTime = 2f;
+    private float startGameWaitingCounter;
 
     private GameObject bossYakkuruSpawner;
 
@@ -38,8 +43,10 @@ public class YakkuruControls : MonoBehaviour
 
 
     private void OnValidate()
-    {
+    { 
         if (runningTimer <= 0) {  runningTimer = 3f; }
+
+        gameJustStarted= true;
     }
 
     // Start is called before the first frame update
@@ -55,91 +62,122 @@ public class YakkuruControls : MonoBehaviour
 
         if (player == null) { player = GameObject.Find("Player"); }
 
-        //cardinalDirections.Add(new Vector2(0, 1));
-        //cardinalDirections.Add(new Vector2(0, -1));
-        //cardinalDirections.Add(new Vector2(1, 0));
-        //cardinalDirections.Add(new Vector2(-1, 0));
+        cardinalDirections= new List<Vector2>();
+        cardinalDirections.Add(new Vector2(0, 1));
+        cardinalDirections.Add(new Vector2(0, -1));
+        cardinalDirections.Add(new Vector2(1, 0));
+        cardinalDirections.Add(new Vector2(-1, 0));
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (isWaiting)
+        //on attend 2f sans rien faire au debut du jeu
+        if (gameJustStarted)
         {
-            waitingTimerStarted= true;
-            spriteRenderer.color= Color.white;
-        }
-        else
-        {
-            walkingTimerStarted= true;
-            spriteRenderer.color = Color.yellow;
-        }
-        
-        if (waitingTimerStarted)
-        {
-            waitingTimerCounter += Time.deltaTime;
-            Debug.Log("I am waiting");
-            if(waitingTimerCounter > waitingTimer) 
+            if (startGameWaitingCounter< startGameWaitingTime) 
             {
-                isWaiting= false;
-                waitingTimerStarted= false;
-                walkingTimerStarted= true;
-                waitingTimerCounter= 0;
+                startGameWaitingCounter += Time.deltaTime;
             }
+            else gameJustStarted= false;
+
+                
         }
 
-        if (walkingTimerStarted)
+        //j'ai mis tout le code dedans pour eviter les bugs de demarrage ca semble fonctionner
+        if (!gameJustStarted)
         {
-            walkingTimerCounter += Time.deltaTime;
-            Debug.Log("I am walking");
-            if (walkingTimerCounter > walkingTimer)
+            //ca c'est le code de l'idle
+            if (isWaiting)
             {
-                isWaiting= true;
-                walkingTimerStarted= false;
-                waitingTimerStarted= true;
-                walkingTimerCounter= 0;
-            }
-        }
-
-
-        if (isFleeing) 
-        {
-            timerStarted = true;
-            //isWalking = false;
-            isWaiting = false; 
-            spriteRenderer.color= Color.white;
-
-            if (!hasFleeingDirection)
-            {
-                fleeingDirection = new Vector2(transform.position.x - player.transform.position.x, 
-                    transform.position.y - player.transform.position.y).normalized;
-                hasFleeingDirection = true;
-                animator.SetBool("isRunning", true);
-            }
-
-            rb2.velocity = fleeingDirection*yakkuruData.runSpeed*Time.deltaTime;
-            if (fleeingDirection.x < 0)
-            {
-                gameObject.transform.localScale = new Vector3(-1, 1, 1); 
-                //msieur Roussel il a dit
-                //pas bien
-                //spriteRenderer.flipX = true;
-            }else { gameObject.transform.localScale = new Vector3(1, 1, 1); }
-        }
-
-        if (timerStarted)
-        {
-            runningCounter += Time.deltaTime;
-            if (runningCounter >= runningTimer)
-            {
-                isFleeing = false;
-                timerStarted = false;
-                runningCounter = 0;
+                hasDirection = false;
+                waitingTimerStarted = true;
+                spriteRenderer.color = Color.white;
                 rb2.velocity = Vector2.zero;
-                animator.SetBool("isRunning", false);
-                isWaiting= true;
+            }
+            else
+            {
+                if (!hasDirection)
+                {
+                    walkingDirection = RandomCardinalDirection();
+                    hasDirection = true;
+                }
+                walkingTimerStarted = true;
+                spriteRenderer.color = Color.yellow;
+                rb2.velocity = walkingDirection * yakkuruData.walkSpeed * Time.deltaTime;
+            }
+
+            //c'est le timer de l'idle
+            if (waitingTimerStarted)
+            {
+                waitingTimerCounter += Time.deltaTime;
+                Debug.Log("I am waiting");
+                if (waitingTimerCounter > waitingTimer)
+                {
+                    isWaiting = false;
+                    waitingTimerStarted = false;
+                    walkingTimerStarted = true;
+                    waitingTimerCounter = 0;
+                }
+            }
+
+            //c'est la marche de l'idle
+            if (walkingTimerStarted)
+            {
+                walkingTimerCounter += Time.deltaTime;
+                Debug.Log("I am walking");
+                if (walkingTimerCounter > walkingTimer)
+                {
+                    isWaiting = true;
+                    walkingTimerStarted = false;
+                    waitingTimerStarted = true;
+                    walkingTimerCounter = 0;
+                }
+            }
+
+
+            //code de la reaction aux degats
+            if (isFleeing)
+            {
+                timerStarted = true;
+                //isWalking = false;
+                isWaiting = false;
+                spriteRenderer.color = Color.white;
+
+                if (!hasFleeingDirection)
+                {
+                    fleeingDirection = new Vector2(transform.position.x - player.transform.position.x,
+                        transform.position.y - player.transform.position.y).normalized;
+                    hasFleeingDirection = true;
+                    animator.SetBool("isRunning", true);
+                }
+
+                rb2.velocity = fleeingDirection * yakkuruData.runSpeed * Time.deltaTime;
+                if (fleeingDirection.x < 0)
+                {
+                    gameObject.transform.localScale = new Vector3(-1, 1, 1);
+                    //msieur Roussel il a dit
+                    //pas bien
+                    //spriteRenderer.flipX = true;
+                }
+                else { gameObject.transform.localScale = new Vector3(1, 1, 1); }
+            }
+
+            if (timerStarted)
+            {
+                runningCounter += Time.deltaTime;
+                if (runningCounter >= runningTimer)
+                {
+                    isFleeing = false;
+                    timerStarted = false;
+                    runningCounter = 0;
+                    rb2.velocity = Vector2.zero;
+                    animator.SetBool("isRunning", false);
+                    isWaiting = true;
+                }
             }
         }
+
     }
 
     private Vector2 RandomCardinalDirection()
