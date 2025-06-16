76b27c86a3dae971709e4a8e6ddb7e29b356a707
FishyTree <53624883+camcartier@users.noreply.github.com>
2025-06-16 13:30:04 +0200

    Scene3 les images changent avec le texte
    as it says in the title

--- Assets/Scripts/UIScripts/NarrationTXTManager.cs
+++ Assets/Scripts/UIScripts/NarrationTXTManager.cs
@@ -8,6 +8,7 @@ public class NarrationTXTManager : MonoBehaviour
     public string[] arrayOfText = new string[6];
     [SerializeField] TextMeshProUGUI textToRead;
     private int currentTXTIndex;
+    [SerializeField] GameObject startImage;
 
     public GameObject[] arrayOfBGImages = new GameObject[6];
 
@@ -15,16 +16,35 @@ public class NarrationTXTManager : MonoBehaviour
     void Start()
     {
         currentTXTIndex = 0;
+
+        if (startImage.activeInHierarchy == false)
+        {
+            startImage.SetActive(true);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         textToRead.text = arrayOfText[currentTXTIndex];
+
+        if (currentTXTIndex > 0)
+        {
+            if (arrayOfBGImages[currentTXTIndex].activeInHierarchy == false)
+            {
+                arrayOfBGImages[currentTXTIndex].SetActive(true);
+            }
+        }
+
     }
 
     public void IncrementIndex()
     {
-        currentTXTIndex += 1;
+        if (currentTXTIndex < arrayOfBGImages.Length -1)
+        {
+            currentTXTIndex += 1; 
+        }
+        else { currentTXTIndex = arrayOfBGImages.Length - 1; }
+
     }
 }
