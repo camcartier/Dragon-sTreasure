1a875f7bd103808c4278ec902a3ef8d56d95fd7e
FishTree <53624883+camcartier@users.noreply.github.com>
2026-01-19 02:14:33 +0100

    alors... enemybump
    j'ai fait un script tout bete qui bump les enemis vers l'arriere quand ils sont touchés par une bullet, mais ça marche sur le soldat et pas sur le voleur et je ne sais pas pourquoi

---
+++ Assets/Scripts/Characters/EnemiesBump.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemiesBump : MonoBehaviour
+{
+    [SerializeField] Rigidbody2D rb2d;
+
+    public float bumpForce;
+    public Vector2 direction;
+
+   
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Bullet"))
+        {
+            Debug.Log("bump should happen");
+            
+            
+            if(collision.transform.position.x > gameObject.transform.position.x)
+            {
+                if (collision.transform.position.y > gameObject.transform.position.y)
+                {
+                    
+                    direction = new Vector2(gameObject.transform.position.x - collision.transform.position.x, gameObject.transform.position.y - collision.transform.position.y);
+                }
+
+                else { direction = new Vector2(gameObject.transform.position.x - collision.transform.position.x, collision.transform.position.y - gameObject.transform.position.y); }
+
+            }
+            else
+            {
+                direction = new Vector2(collision.transform.position.x - gameObject.transform.position.x, gameObject.transform.position.y - collision.transform.position.y);
+            }
+
+                rb2d.AddForce(direction * bumpForce);
+        }
+    }
+
+
+
+}
