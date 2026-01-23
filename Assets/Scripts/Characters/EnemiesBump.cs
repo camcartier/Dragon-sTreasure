55f679f9168f695beac2c8c223e496b80da9c6ac
FishyTree <53624883+camcartier@users.noreply.github.com>
2026-01-23 12:38:56 +0100

    trying to correct bump
    new results on this pc, have to try make everything work despite inconsistent framerate

--- Assets/Scripts/Characters/EnemiesBump.cs
+++ Assets/Scripts/Characters/EnemiesBump.cs
@@ -10,35 +10,74 @@ public class EnemiesBump : MonoBehaviour
     public Vector2 direction;
 
     [SerializeField] WalkTowards walkTowards;
-   
+
+    public float timeForBump;
+    public float storedTimeForBump;
+    private float timerCouterBump;
+
+    public bool hasBeenBumped;
+
+    private void Update()
+    {
+        if (walkTowards.isFollowing == false)
+        {
+            rb2d.velocity = Vector2.zero;
+            rb2d.AddForce(direction * bumpForce, ForceMode2D.Impulse );
+
+
+        }
+
+        if (timerCouterBump < timeForBump)
+        {
+            timerCouterBump += Time.deltaTime;
+        }
+        else
+        {
+            walkTowards.isFollowing = true;
+            hasBeenBumped = false;
+            timerCouterBump = 0f;
+        }
+
+    }
+
+
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Bullet"))
         {
             Debug.Log("bump should happen");
-            
-            
-            if(collision.transform.position.x > gameObject.transform.position.x)
+
+            hasBeenBumped = true;
+            if (collision.transform.position.x > gameObject.transform.position.x)
             {
                 if (collision.transform.position.y > gameObject.transform.position.y)
                 {
                     
-                    direction = new Vector2(gameObject.transform.position.x - collision.transform.position.x, gameObject.transform.position.y - collision.transform.position.y);
+                    direction = new Vector2(gameObject.transform.position.x - collision.transform.position.x, gameObject.transform.position.y - collision.transform.position.y).normalized;
                 }
 
-                else { direction = new Vector2(gameObject.transform.position.x - collision.transform.position.x, collision.transform.position.y - gameObject.transform.position.y); }
+                else { direction = new Vector2(gameObject.transform.position.x - collision.transform.position.x, collision.transform.position.y - gameObject.transform.position.y).normalized; }
 
             }
             else
             {
-                direction = new Vector2(collision.transform.position.x - gameObject.transform.position.x, gameObject.transform.position.y - collision.transform.position.y);
+                direction = new Vector2(collision.transform.position.x - gameObject.transform.position.x, gameObject.transform.position.y - collision.transform.position.y).normalized;
             }
 
-                rb2d.AddForce(direction * bumpForce);
+            walkTowards.isFollowing = false;
+                //rb2d.AddForce(direction * bumpForce, ForceMode2D.Impulse);
+        }
+
+        if (collision.gameObject.CompareTag("Enemies"))
+        {
+            if (hasBeenBumped == true)
+            {
+                collision.gameObject.GetComponent<WalkTowards>().isFollowing = false;
+            }
+            
         }
     }
-
 
 
 }
